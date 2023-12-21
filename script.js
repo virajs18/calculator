851b6e22e9c11650c0fd2d7001288a2cd13dd71f
virajs18 <virajsonar18@gmail.com>
2023-12-21 11:28:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.querySelector('.DisplayC');
+const buttons = document.querySelectorAll('.button');
+let String = "";
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        
+        if(e.target.innerHTML == '='){
+            String = eval(String);
+            display.innerHTML = String;
+            String = "";
+        }
+        else if(e.target.innerHTML == 'C'){
+            String = "0";
+            display.innerHTML = String;
+            String = "";
+        }
+        else if(e.target.innerHTML == 'x'){
+            String+="*";
+            display.innerHTML = String;
+        }
+        else{
+        String += e.target.innerHTML;
+        display.innerHTML = String;
+        }
+    })
+})
